aff98fd96bba331b3f8bcf397e3daf0a826fd5c9
miklosmayer <48197858+miklosmayer@users.noreply.github.com>
2026-04-27 16:43:52 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,13 +7,13 @@ export const SITE: Site = {
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "hu", // html lang code. Set this empty and default will be "en"
+  langTag: [], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
