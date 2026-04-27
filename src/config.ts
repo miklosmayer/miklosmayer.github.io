4c49390cbf7e87a00af585bcfd04829a65f837c7
miklosmayer <48197858+miklosmayer@users.noreply.github.com>
2026-04-27 14:47:41 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  website: "https://miklosmayer.github.io/astronew/", // replace this with your deployed domain
+  author: "Miklos Mayer",
+  desc: "Fizikatanárki tapasztalataim",
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
