c69d643116dd0156208490f3a11dcaaaa93f41b0
miklosmayer <48197858+miklosmayer@users.noreply.github.com>
2026-04-27 15:22:37 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://miklosmayer.github.io/astronew/", // replace this with your deployed domain
+  website: "https://miklosmayer.github.io", // replace this with your deployed domain
   author: "Miklos Mayer",
   desc: "Fizikatanári tapasztalataim",
   title: "AstroPaper",
